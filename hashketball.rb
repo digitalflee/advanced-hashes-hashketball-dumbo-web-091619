36ddb48ec192d223ceafbeb40641a50884c245a8
Keenan Alves <keenan.alves@gmail.com>
2019-09-15 22:39:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,8 +157,9 @@ end
 
 def team_colors(team_name)
   game_hash.each do |loc, team|
-    binding.pry 
-    
+    if team[:team_name] == team_name
+      return team[:colors]
+    end 
   end 
 end 
 
